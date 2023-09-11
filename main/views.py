9ba4d48705d1d2f5b2416b91a91363a8bab33886
CyberSleeper <mahartha.gemilang@gmail.com>
2023-09-11 14:45:11 +0700

    add name and class in views

--- main/views.py
+++ main/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 def show_product(request):
   context = {
+    'nama_mahasiswa': 'Mahartha Gemilang',
+    'kelas_mahasiswa': 'PBP D',
     'name': 'Cheese Sauce',
     'amount': '127',
     'description': 'A product made of sauce'
