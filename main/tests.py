ed2069e37f50228cc96235da0b54e37da47d723e
CyberSleeper <mahartha.gemilang@gmail.com>
2023-09-12 19:34:34 +0700

    add tests

--- main/tests.py
+++ main/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase, Client
+from main.models import Item
 
 class mainTest(TestCase):
   def test_main_url_is_exist(self):
@@ -8,3 +9,10 @@ class mainTest(TestCase):
   def test_main_using_main_template(self):
     response = Client().get('/main/')
     self.assertTemplateUsed(response, 'main.html')
+
+  def test_create_object(self):
+    Item.objects.create(name='Cheese Sauce', amount=127, description='A product made of sauce')
+    Cheese_Sauce = Item.objects.get(name='Cheese Sauce')
+    self.assertEqual(Cheese_Sauce.name, 'Cheese Sauce')
+    self.assertEqual(Cheese_Sauce.amount, 127)
+    self.assertEqual(Cheese_Sauce.description, 'A product made of sauce')
