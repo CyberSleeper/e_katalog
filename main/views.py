a1c37d446744110ea5473b4e76d2944f6e9b120d
CyberSleeper <mahartha.gemilang@gmail.com>
2023-09-20 00:30:28 +0700

    [FIX] get by id

--- main/views.py
+++ main/views.py
@@ -33,7 +33,7 @@ def show_xml(request):
   data = Item.objects.all()
   return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
-def show_xml_by_id(request):
+def show_xml_by_id(request, id):
   data = Item.objects.filter(pk=id)
   return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
@@ -41,6 +41,6 @@ def show_json(request):
   data = Item.objects.all()
   return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
-def show_json_by_id(request):
+def show_json_by_id(request, id):
   data = Item.objects.filter(pk=id)
   return HttpResponse(serializers.serialize("json", data), content_type="application/json")
