b0970d1755761649f25a4cdfc4c8f45b14440ae3
CyberSleeper <mahartha.gemilang@gmail.com>
2023-09-25 13:51:37 +0700

    [ADD] Week3

--- main/urls.py
+++ main/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
-from main.views import show_main, create_item, show_xml, show_xml_by_id, show_json, show_json_by_id
+from main.views import show_main, create_item, show_xml, show_xml_by_id, show_json, show_json_by_id, register, login_user, logout_user
 
 app_name = 'main'
 
 urlpatterns = [
   path('', show_main, name='show_main'),
-  path('create-item', create_item, name='create_item'),
+  path('create-item/', create_item, name='create_item'),
   path('xml/', show_xml, name='show_xml'),
   path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
   path('json/', show_json, name='show_json'),
   path('json/<int:id>/', show_json_by_id, name='show_json_by_id'),
+  path('register/', register, name='register'),
+  path('login/', login_user, name='login'),
+  path('logout/', logout_user, name='logout')
 ]
--- main/views.py
+++ main/views.py
@@ -1,13 +1,21 @@
-from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
-from main.forms import ItemForm
-from django.urls import reverse
-from main.models import Item
-from django.core import serializers
+import datetime
 
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.core import serializers
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
+from main.forms import ItemForm
+from main.models import Item
+
+@login_required(login_url='/login')
 def show_main(request):
-  items = Item.objects.all()
-  count = Item.objects.all().count()
+  items = Item.objects.filter(user=request.user)
+  count = Item.objects.filter(user=request.user).count()
 
   context = {
     'nama_mahasiswa': 'Mahartha Gemilang',
@@ -16,7 +24,9 @@ def show_main(request):
     'amount': '127',
     'description': 'A product made of sauce',
     'count': count,
-    'items': items
+    'items': items,
+    'current_user': request.user,
+    'last_login': request.COOKIES['last_login'],
   }
 
   return render(request, 'main.html', context)
@@ -25,7 +35,9 @@ def create_item(request):
   form = ItemForm(request.POST or None)
 
   if form.is_valid() and request.method == "POST":
-    form.save()
+    item = form.save(commit=False)
+    item.user = request.user
+    item.save()
     return HttpResponseRedirect(reverse('main:show_main'))
   
   context = {'form': form}
@@ -46,3 +58,36 @@ def show_json(request):
 def show_json_by_id(request, id):
   data = Item.objects.filter(pk=id)
   return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def register(request):
+  form = UserCreationForm()
+
+  if request.method == "POST":
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      form.save()
+      messages.success(request, 'Your account has been successfully created!')
+      return redirect('main:login')
+  context = {'form':form}
+  return render(request, 'register.html', context)
+  
+def login_user(request):
+  if request.method == 'POST':
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+      login(request, user)
+      response = HttpResponseRedirect(reverse("main:show_main")) 
+      response.set_cookie('last_login', str(datetime.datetime.now()))
+      return response
+    else:
+      messages.info(request, 'Sorry, incorrect username or password. Please try again.')
+  context = {}
+  return render(request, 'login.html', context)
+
+def logout_user(request):
+  logout(request)
+  response = HttpResponseRedirect(reverse('main:login'))
+  response.delete_cookie('last_login')
+  return redirect('main:login')
