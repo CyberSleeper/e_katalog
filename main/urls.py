3ca67e798fd95a192d15d92c4cddb4667c526219
CyberSleeper <mahartha.gemilang@gmail.com>
2023-10-08 15:10:52 +0700

    collectStatic-less

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, create_item, show_xml, show_xml_by_id, show_json, show_json_by_id, register, login_user, logout_user, increment_counter, decrement_counter, delete_item
+from main.views import show_main, create_item, show_xml, show_xml_by_id, show_json, show_json_by_id, register, login_user, logout_user, increment_counter, decrement_counter, delete_item, get_item_json, add_item_ajax
 
 app_name = 'main'
 
@@ -15,5 +15,7 @@ urlpatterns = [
   path('logout/', logout_user, name='logout'),
   path('increment/<int:id>/', increment_counter, name='increment_counter'),
   path('decrement/<int:id>/', decrement_counter, name='decrement_counter'),
-  path('delete/<int:id>/', delete_item, name='delete_item')
+  path('delete/<int:id>/', delete_item, name='delete_item'),
+  path('get-item/', get_item_json, name='get_item_json'),
+  path('create-ajax/', add_item_ajax, name='add_item_ajax')
 ]
--- main/views.py
+++ main/views.py
@@ -5,9 +5,10 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.core import serializers
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotFound
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
 
 from main.forms import ItemForm
 from main.models import Item
@@ -108,3 +109,22 @@ def delete_item(request, id):
   item = (Item.objects.get(id=id))
   item.delete()
   return redirect('main:show_main')
+
+def get_item_json(request):
+  items = Item.objects.all()
+  return HttpResponse(serializers.serialize('json', items))
+
+@csrf_exempt
+def add_item_ajax(request):
+  if request.method == 'POST':
+    name = request.POST.get("name")
+    amount = request.POST.get("amount")
+    description = request.POST.get("description")
+    user = request.user
+
+    new_item = Item(name=name, amount=amount, description=description, user=user)
+    new_item.save()
+
+    return HttpResponse(b"CREATED", STATUS=201)
+  
+  return HttpResponseNotFound()
